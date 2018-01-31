3dc4d9c644a0be4a0cdce80c4064ac1435f0aac7
Adrian Hofman <Adrian.Hofman@recordpoint.com>
2018-01-31 16:37:27 +1100

    Provide implementations for performance and performanceevent

---
+++ Source/RecordPoint.Connectors.SDK/Diagnostics/Performance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RecordPoint.Connectors.SDK.Diagnostics
+{
+    public class Performance : IPerformance
+    {
+        public ILog Log { get; set; } = null;
+
+        public IPerformanceEvent CreateEvent(Type type, string methodName, string message)
+        {
+            return new PerformanceEvent(type, methodName, message, Log);
+        }
+    }
+}
---
+++ Source/RecordPoint.Connectors.SDK/Diagnostics/PerformanceEvent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RecordPoint.Connectors.SDK.Diagnostics
+{
+    internal class PerformanceEvent : IPerformanceEvent
+    {
+        private readonly string _message;
+        private readonly Stopwatch _stopWatch = new Stopwatch();
+        private readonly ILog _log = null;
+        private readonly Type _type;
+        private readonly string _method;
+        private Exception _ex;
+
+        public PerformanceEvent(Type type, string method, string message, ILog log)
+        {
+            _message = message;
+            _log = log;
+            _type = type;
+            _method = method;
+            _stopWatch.Start();
+        }
+
+        public void Exception(Exception ex)
+        {
+            _ex = ex;
+        }
+
+        public void Dispose()
+        {
+            // stop existing stop watch
+            _stopWatch.Stop();
+            
+            var message = _message;
+            var method = _method;
+
+            if (_ex != null)
+            {
+                method += "_Faulted";
+                message += " " + _ex.ToString();
+            }
+
+            // log timing event in ticks
+            _log?.LogTimingEventTicks(_type, method, _stopWatch.Elapsed.Ticks, message);
+        }
+    }
+}
