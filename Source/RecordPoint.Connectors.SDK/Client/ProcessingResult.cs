c4711cebcd89dbf83c835bfa8326f22f4413b7df
Adrian Hofman <Adrian.Hofman@recordpoint.com>
2018-02-23 08:38:32 +1100

    Add strongly typed enum for ProcesingResult Add an extension method on ConnectorNotificationModel to create an acknowledge model from it

---
+++ Source/RecordPoint.Connectors.SDK/Client/ProcessingResult.cs
@@ -0,0 +1,21 @@
+﻿namespace RecordPoint.Connectors.SDK.Client
+{
+    /// <summary>
+    /// Describes the results a processing operation for a connector notification.
+    /// </summary>
+    public enum ProcessingResult
+    {
+        /// <summary>
+        /// Indicates that the required processing for the notification completed successfully.
+        /// </summary>
+        OK,
+        Unknown,
+        ConnectorDisabled,
+        ConnectorNotSubscribed,
+        ConnectorNotReachable,
+        /// <summary>
+        /// An error occured during processing of a notification.
+        /// </summary>
+        NotificationError
+    }
+}
