793e3cd42aa64c57abfdecdcd45cb64058a6b8b1
Adrian Hofman <Adrian.Hofman@recordpoint.com>
2018-04-16 03:18:39 +0000

    Merged PR 3798: IConnectorConfigAccessor + some useful helpers
    IConnectorConfigAccessor + some useful helpers
    
    Related work items: #228022

---
+++ Source/RecordPoint.Connectors.SDK/Client/ConnectorConfigStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace RecordPoint.Connectors.SDK.Client
+{
+    public static class ConnectorConfigStatus
+    {
+        public static string Enabled = nameof(Enabled);
+        public static string Disabled = nameof(Disabled);
+        public static string Error = nameof(Error);
+    }
+}
--- Source/RecordPoint.Connectors.SDK/ValidationHelper.cs
+++ Source/RecordPoint.Connectors.SDK/ValidationHelper.cs
@@ -6,10 +6,9 @@ namespace RecordPoint.Connectors.SDK
     static class ValidationHelper
     {
         /// <summary>
-        /// Throws an exception if the tested string argument is null or an empty string.
+        /// Throws an exception if the tested string argument is null or a string that contains only whitespace.
         /// </summary>
-        /// <exception cref="ArgumentNullException">The string value is null.</exception>
-        /// <exception cref="ArgumentException">The string is empty.</exception>
+        /// <exception cref="ArgumentNullException">The string value is null or contains only whitespace.</exception>
         /// <param name="argumentValue">The argument value to test.</param>
         /// <param name="argumentName">The name of the argument to test.</param>
         public static void ArgumentNotNullOrWhiteSpace(string argumentValue, string argumentName)
@@ -21,17 +20,30 @@ namespace RecordPoint.Connectors.SDK
         }
 
         /// <summary>
+        /// Throws an exception if the tested string argument is null or an empty string.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">The string value is null or empty.</exception>
+        /// <param name="argumentValue">The argument value to test.</param>
+        /// <param name="argumentName">The name of the argument to test.</param>
+        public static void ArgumentNotNullOrEmpty(string argumentValue, string argumentName)
+        {
+            if (string.IsNullOrEmpty(argumentValue))
+            {
+                throw new ArgumentNullException($"{argumentName} cannot be null or empty.");
+            }
+        }
+
+        /// <summary>
         /// Throws an exception if the tested SecureString argument is null or an empty string.
         /// </summary>
         /// <exception cref="ArgumentNullException">The string value is null.</exception>
-        /// <exception cref="ArgumentException">The string is empty.</exception>
         /// <param name="argumentValue">The argument value to test.</param>
         /// <param name="argumentName">The name of the argument to test.</param>
         public static void ArgumentNotNullOrEmpty(SecureString argumentValue, string argumentName)
         {
             if ((argumentValue == null) || (argumentValue.Length == 0))
             {
-                throw new ArgumentNullException($"{argumentName} cannot be null, empty, or only whitespace.");
+                throw new ArgumentNullException($"{argumentName} cannot be null or empty.");
             }
         }
 
