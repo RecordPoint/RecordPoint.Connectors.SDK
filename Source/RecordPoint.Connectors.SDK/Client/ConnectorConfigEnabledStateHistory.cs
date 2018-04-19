7cc54be41e98627761291c4f8d497af826259121
Adrian Hofman <Adrian.Hofman@recordpoint.com>
2018-04-19 02:03:30 +0000

    Merged PR 3832: Add a constant for HasBeenEnabled
    Add a constant for HasBeenEnabled
    
    Related work items: #228022

---
+++ Source/RecordPoint.Connectors.SDK/Client/ConnectorConfigEnabledStateHistory.cs
@@ -0,0 +1,7 @@
+﻿namespace RecordPoint.Connectors.SDK.Client
+{
+    public static class ConnectorConfigEnabledStateHistory
+    {
+        public static string HasBeenEnabled = nameof(HasBeenEnabled);
+    }
+}
