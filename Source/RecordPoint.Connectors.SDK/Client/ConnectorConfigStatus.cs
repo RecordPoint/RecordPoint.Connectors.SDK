3c9a3b906fdbc4b8b803fd440cc41c41d0fa1dd6
Adrian Hofman <Adrian.Hofman@recordpoint.com>
2018-08-02 23:36:20 +0000

    Merged PR 4704: Fix incorrect enum, add comments
    Fix incorrect NotificationType enum - "ConnectorConfigAdded" doesn't exist and should be ConnectorConfigCreated instead
    Added a bunch of documentation
    
    Related work items: #231604

--- Source/RecordPoint.Connectors.SDK/Client/ConnectorConfigStatus.cs
+++ Source/RecordPoint.Connectors.SDK/Client/ConnectorConfigStatus.cs
@@ -1,9 +1,26 @@
 ﻿namespace RecordPoint.Connectors.SDK.Client
 {
+    /// <summary>
+    /// Defines constant values that may appear in the ConnectorConfigModel.Status field.
+    /// </summary>
     public static class ConnectorConfigStatus
     {
+        /// <summary>
+        /// Indicates the Connector is in the Enabled status.
+        /// The connector is expected to operate as normal in this status.
+        /// </summary>
         public const string Enabled = nameof(Enabled);
+
+        /// <summary>
+        /// Indicates the connector is in the Disabled status.
+        /// The connector is expected to do nothing in this status.
+        /// </summary>
         public const string Disabled = nameof(Disabled);
+
+        /// <summary>
+        /// Indicates the connector is in the Error status.
+        /// The connector is expected to do nothing in this status.
+        /// </summary>
         public const string Error = nameof(Error);
     }
 }
