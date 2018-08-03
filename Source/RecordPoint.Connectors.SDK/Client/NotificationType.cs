ff482d5d42f28e3279d7f4a3f5e6a04faa02a710
Adrian Hofman <Adrian.Hofman@recordpoint.com>
2018-08-03 10:10:30 +1000

    code drop

--- Source/RecordPoint.Connectors.SDK/Client/NotificationType.cs
+++ Source/RecordPoint.Connectors.SDK/Client/NotificationType.cs
@@ -1,16 +1,46 @@
 ﻿namespace RecordPoint.Connectors.SDK.Client
 {
     /// <summary>
-    /// Records365vNext NotificationType. 
-    /// Possible values include: 'ItemDestroyed', 'Ping', 'ConnectorConfigAdded', 'ConnectorConfigUpdated', 'ConnectorConfigDeleted' and 'Unknown'
+    /// Defines constant values that may appear in the ConnectorNotificationModel.NotificationType field.
+    /// Possible values include: 'ItemDestroyed', 'Ping', 'ConnectorConfigCreated', 'ConnectorConfigUpdated', and 'ConnectorConfigDeleted'
     /// </summary>
-    public enum NotificationType
+    public static class NotificationType
     {
-        Unknown,
-        ItemDestroyed,
-        Ping,
-        ConnectorConfigAdded,
-        ConnectorConfigUpdated,
-        ConnectorConfigDeleted
+        /// <summary>
+        /// The ItemDestroyed Notification Type. 
+        /// This notification is sent by Records365 vNext when an item is disposed
+        /// in the platform.
+        /// The connector must permanently destroy all metadata, binaries and ay other 
+        /// information associated with the item in the content source.
+        /// </summary>
+        public const string ItemDestroyed = nameof(ItemDestroyed);
+
+        /// <summary>
+        /// The Ping Notification Type.
+        /// Used for testing purposes only.
+        /// </summary>
+        public const string Ping = nameof(Ping);
+
+        /// <summary>
+        /// The ConnectorConfigCreated Notification Type.
+        /// This notification is sent by Records365 vNext when a new instance of the
+        /// connector is created in the platform.
+        /// </summary>
+        public const string ConnectorConfigCreated = nameof(ConnectorConfigCreated);
+
+        /// <summary>
+        /// The ConnectorConfigUpdated Notification Type.
+        /// This notification is sent by Records365 vNext when an existing instance of
+        /// the connector is updated in the platform. Possible updates may include
+        /// configuration changes, or the connector being enabled or disabled by a user.
+        /// </summary>
+        public const string ConnectorConfigUpdated = nameof(ConnectorConfigUpdated);
+
+        /// <summary>
+        /// The ConnectorConfigDeleted Notification Type.
+        /// This notification is sent by Records365 vNext when an instance of the 
+        /// connector is deleted in the platform.
+        /// </summary>
+        public const string ConnectorConfigDeleted = nameof(ConnectorConfigDeleted);
     }
 }
